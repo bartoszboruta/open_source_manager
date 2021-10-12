c70f64610e19ca6c6a7796e0682ec765d95d8148
Wojciech Bożek <bozek.wojciech@gmail.com>
2021-10-12 11:43:35 +0200

    Dla Marka <3

--- src/screens/MainStack/MainStack.tsx
+++ src/screens/MainStack/MainStack.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "./Profile";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Issues") {
+          iconName = "albums-outline";
+        } else if (route.name === "Profile") {
+          iconName = "person-circle-outline";
+        } else if (route.name === "Ideas") {
+          iconName = "bulb-outline";
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: "tomato",
+      tabBarInactiveTintColor: "gray",
+    })}
+  >
     <Tab.Screen name="Issues" component={() => null} />
     <Tab.Screen name="Ideas" component={() => null} />
     <Tab.Screen name="Profile" component={Profile} />
