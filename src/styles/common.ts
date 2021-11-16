93fab8360835db5ac110ff3172b3d4e14ae1cfb6
bartoszboruta <bboruta.dev@gmail.com>
2021-11-16 15:43:59 +0100

    src/styles

---
+++ src/styles/common.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import spacing from "./spacing";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.xs,
+  },
+});
+
+export default styles;
