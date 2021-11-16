4a99665fcdec8b84b0095366c01da4ae10f450f5
matro <mark.troszczynski@gmail.com>
2021-11-16 16:24:57 +0100

    initial setup for profile page

--- src/screens/MainStack/Profile/Profile.tsx
+++ src/screens/MainStack/Profile/Profile.tsx
@@ -1,69 +1,39 @@
 import React from "react";
-import { Button, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import { styles } from "./styles";
 
-import { useAppDispatch } from "store";
-import { setCredentials } from "store/auth/authSlice";
-import { clearStorage, getToken, getUserData } from "utils";
+import { useFetchProfileQuery } from "../../../store/github/slice";
 
 const Profile = () => {
-  const dispatch = useAppDispatch();
+  const { data, error, isLoading } = useFetchProfileQuery({
+    name: "marektro",
+  });
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.loader}>
+          data is being loaded, have patience my friend
+        </Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>
+          Found an error: {JSON.stringify(error)}
+        </Text>
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Button
-        title="CheckTokens"
-        onPress={async () => {
-          const token = await getToken();
-          const userData = await getUserData();
-          console.log("STORED TOKEN", token);
-          console.log("STORED USER DATA", userData);
-        }}
-      />
-      <Button
-        title="ClearTokens"
-        onPress={async () => {
-          await clearStorage();
-          dispatch(setCredentials({ token: "", user: null }));
-        }}
-      />
+    <View style={styles.container}>
+      <Text style={styles.handle}>{data.login}</Text>
+      <Image style={styles.avatar} source={{ uri: data.avatar_url }}></Image>
+      <Text style={styles.name}>{data.name}</Text>
     </View>
   );
 };
-
 export default Profile;
-const apiResponse = {
-  login: "octocat",
-  id: 1,
-  node_id: "MDQ6VXNlcjE=",
-  avatar_url: "https://github.com/images/error/octocat_happy.gif",
-  gravatar_id: "",
-  url: "https://api.github.com/users/octocat",
-  html_url: "https://github.com/octocat",
-  followers_url: "https://api.github.com/users/octocat/followers",
-  following_url: "https://api.github.com/users/octocat/following{/other_user}",
-  gists_url: "https://api.github.com/users/octocat/gists{/gist_id}",
-  starred_url: "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-  subscriptions_url: "https://api.github.com/users/octocat/subscriptions",
-  organizations_url: "https://api.github.com/users/octocat/orgs",
-  repos_url: "https://api.github.com/users/octocat/repos",
-  events_url: "https://api.github.com/users/octocat/events{/privacy}",
-  received_events_url: "https://api.github.com/users/octocat/received_events",
-  type: "User",
-  site_admin: false,
-  name: "monalisa octocat",
-  company: "GitHub",
-  blog: "https://github.com/blog",
-  location: "San Francisco",
-  email: "octocat@github.com",
-  hireable: false,
-  bio: "There once was...",
-  twitter_username: "monatheoctocat",
-  public_repos: 2,
-  public_gists: 1,
-  followers: 20,
-  following: 0,
-  created_at: "2008-01-14T04:33:35Z",
-  updated_at: "2008-01-14T04:33:35Z",
-};
-
-export default () => <Text>tu byłem - Tony Halik</Text>;
