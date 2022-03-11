fd18025ef715339a3e4f641b5df638cae14fcc17
Tomasz Kielar <32620457+k1eu@users.noreply.github.com>
2022-03-11 11:34:28 +0100

    feat: add profile info & logout (#30)
    * feat: add user fetching and logout to profile
    
    * feat: display created and assigned values on profile

--- src/screens/MainStack/Profile/Profile.tsx
+++ src/screens/MainStack/Profile/Profile.tsx
@@ -1,39 +1,63 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity, SafeAreaView } from "react-native";
+import { useSelector } from "react-redux";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+import { useAuth } from "hooks";
+import { useFetchProfileQuery } from "../../../store/github/slice";
+import { selectCurrentUser } from '../../../store/auth/authSlice';
+
+import Issues from './Analitycs/Issues'
 import { styles } from "./styles";
 
-import { useFetchProfileQuery } from "../../../store/github/slice";
-
 const Profile = () => {
+  const { resetCredentials } = useAuth();
+  const user = useSelector(selectCurrentUser)
   const { data, error, isLoading } = useFetchProfileQuery({
-    name: "marektro",
+    name: user!.github_name
   });
+
+  const handleLogout = () => {
+    resetCredentials();
+  }
+
+
   if (isLoading) {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.loader}>
           data is being loaded, have patience my friend
         </Text>
-      </View>
+      </SafeAreaView>
     );
   }
 
   if (error) {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.error}>
           Found an error: {JSON.stringify(error)}
         </Text>
-      </View>
+      </SafeAreaView>
     );
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.handle}>{data.login}</Text>
-      <Image style={styles.avatar} source={{ uri: data.avatar_url }}></Image>
-      <Text style={styles.name}>{data.name}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}> 
+      <View style={styles.headerUser}>
+        <Image style={styles.avatar} source={{ uri: data.avatar_url }}></Image>
+        <Text style={styles.handle}>{data.login}</Text>
+      </View>
+      <TouchableOpacity onPress={handleLogout}>
+        <Ionicons name="log-out-outline" size={30} color="black" />
+      </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.name}>{data.name}</Text>
+        <Issues />
+      </View>
+    </SafeAreaView>
   );
 };
 export default Profile;
--- src/screens/MainStack/Profile/styles.ts
+++ src/screens/MainStack/Profile/styles.ts
@@ -1,26 +1,39 @@
 import { StyleSheet } from "react-native";
 
+const PADDING = 12;
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    justifyContent: "center",
-    alignItems: "center",
+    position: 'relative'
   },
   error: {
     fontSize: 22,
     color: "red",
   },
   avatar: {
-    height: 200,
-    width: 200,
+    height: 100,
+    width: 100,
     borderRadius: 100,
     backgroundColor: "red",
   },
+  body: {
+    padding: PADDING,
+  },
+  header: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: PADDING
+  },
+  headerUser: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
   handle: {
-    fontSize: 60,
     color: "gray",
-    marginBottom: 30,
+    fontSize: 30,
+    marginLeft: 10
   },
   name: {
     fontSize: 30,
