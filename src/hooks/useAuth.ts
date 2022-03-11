66c28545eefdcc8853fd015400c85e79da60963c
Tomasz Kielar <git@k1eu.dev>
2022-03-11 10:51:24 +0100

    feat: add user fetching and logout to profile

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,14 +3,16 @@ import { useContext } from "react";
 import { AuthContext, AuthContextProps } from "../contexts";
 
 type ReturnDataType = {
+  resetCredentials: () => void;
   token: string | null;
 };
 
 export const useAuth: () => ReturnDataType = () => {
-  const { token } = useContext(AuthContext) as AuthContextProps;
+  const { token, resetCredentials } = useContext(AuthContext) as AuthContextProps;
 
   const returnObject: ReturnDataType = {
     token,
+    resetCredentials
   };
 
   return returnObject;
--- src/screens/MainStack/Profile/Profile.tsx
+++ src/screens/MainStack/Profile/Profile.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
-import { styles } from "./styles";
+import { Text, View, Image, TouchableOpacity, SafeAreaView } from "react-native";
+import { useSelector } from "react-redux";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import { useFetchProfileQuery } from "../../../store/github/slice";
+import { selectCurrentUser } from '../../../store/auth/authSlice';
+
+import { styles } from "./styles";
+import { useAuth } from "hooks";
 
 const Profile = () => {
+  const { resetCredentials } = useAuth();
+  const user = useSelector(selectCurrentUser)
   const { data, error, isLoading } = useFetchProfileQuery({
-    name: "marektro",
+    name: user!.github_name
   });
+
+  const handleLogout = () => {
+    resetCredentials();
+  }
+
+
   if (isLoading) {
     return (
       <View style={styles.container}>
@@ -29,11 +42,20 @@ const Profile = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.handle}>{data.login}</Text>
-      <Image style={styles.avatar} source={{ uri: data.avatar_url }}></Image>
-      <Text style={styles.name}>{data.name}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}> 
+      <View style={styles.headerUser}>
+        <Image style={styles.avatar} source={{ uri: data.avatar_url }}></Image>
+        <Text style={styles.handle}>{data.login}</Text>
+      </View>
+      <TouchableOpacity onPress={handleLogout}>
+        <Ionicons name="log-out-outline" size={30} color="black" />
+      </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.name}>{data.name}</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 export default Profile;
